71d41276b603917030f561a2e14cd178578e2296
fand <mail@amagi.dev>
2022-11-27 23:32:35 +0900

    add workaround??

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use notan::draw::*;
 use notan::prelude::*;
+use workaround::Workaround;
+
+mod workaround;
 
 const W: f32 = 800.0;
 const H: f32 = 600.0;
@@ -9,8 +12,10 @@ const TEX_H: f32 = 800.0;
 #[derive(AppState)]
 struct State {
     textures: Vec<Texture>,
+    rt0: RenderTexture,
     rt1: RenderTexture,
     rt2: RenderTexture,
+    workaround: Workaround,
 }
 
 #[notan_main]
@@ -27,6 +32,10 @@ fn init(gfx: &mut Graphics) -> State {
         texture(gfx, include_bytes!("ferris_transparent.png")),
     ];
 
+    let rt0 = gfx
+        .create_render_texture(TEX_W as i32, TEX_H as i32)
+        .build()
+        .unwrap();
     let rt1 = gfx
         .create_render_texture(TEX_W as i32, TEX_H as i32)
         .build()
@@ -36,7 +45,15 @@ fn init(gfx: &mut Graphics) -> State {
         .build()
         .unwrap();
 
-    State { textures, rt1, rt2 }
+    let workaround = Workaround::new(gfx);
+
+    State {
+        textures,
+        rt0,
+        rt1,
+        rt2,
+        workaround,
+    }
 }
 
 fn texture(gfx: &mut Graphics, img: &[u8]) -> Texture {
@@ -66,33 +83,38 @@ fn draw(app: &mut App, gfx: &mut Graphics, state: &mut State) {
 
     // Draw transparent PNG with RenderTexture
     {
-        let mut d = gfx.create_draw();
-        d.set_size(TEX_W, TEX_H);
-        d.clear(Color::TRANSPARENT);
-        d.image(&state.textures[1])
-            .scale_from((TEX_W / 2.0, TEX_H / 2.0), (1.0, -1.0));
-        gfx.render_to(&state.rt1, &d);
+        state
+            .workaround
+            .draw(&mut gfx.device, &state.textures[1], &state.rt0, vec![1.0]);
 
-        draw.image(&state.rt1)
+        draw.image(&state.rt0)
             .translate(0.0, H / 2.0)
             .scale(scale, scale);
     }
 
     // Draw transparent PNG with RenderTexture twice
     {
-        let mut d1 = gfx.create_draw();
-        d1.set_size(TEX_W, TEX_H);
-        d1.clear(Color::TRANSPARENT);
-        d1.image(&state.textures[1])
-            .scale_from((TEX_W / 2.0, TEX_H / 2.0), (1.0, -1.0));
-        gfx.render_to(&state.rt1, &d1);
+        state
+            .workaround
+            .draw(&mut gfx.device, &state.textures[1], &state.rt1, vec![1.0]);
 
-        let mut d2 = gfx.create_draw();
-        d2.set_size(TEX_W, TEX_H);
-        d2.clear(Color::TRANSPARENT);
-        d2.image(&state.rt1)
-            .scale_from((TEX_W / 2.0, TEX_H / 2.0), (1.0, -1.0));
-        gfx.render_to(&state.rt2, &d2);
+        state
+            .workaround
+            .draw(&mut gfx.device, &state.rt1, &state.rt2, vec![1.0]);
+
+        // let mut d1 = gfx.create_draw();
+        // d1.set_size(TEX_W, TEX_H);
+        // d1.clear(Color::TRANSPARENT);
+        // d1.image(&state.textures[1])
+        //     .scale_from((TEX_W / 2.0, TEX_H / 2.0), (1.0, -1.0));
+        // gfx.render_to(&state.rt1, &d1);
+
+        // let mut d2 = gfx.create_draw();
+        // d2.set_size(TEX_W, TEX_H);
+        // d2.clear(Color::TRANSPARENT);
+        // d2.image(&state.rt1)
+        //     .scale_from((TEX_W / 2.0, TEX_H / 2.0), (1.0, -1.0));
+        // gfx.render_to(&state.rt2, &d2);
 
         draw.image(&state.rt2)
             .translate(width, H / 2.0)
---
+++ src/workaround.rs
@@ -0,0 +1,132 @@
+use notan::prelude::*;
+
+//language=glsl
+const VERTEX_SHADER: ShaderSource = notan::vertex_shader! {
+  r#"
+    #version 450
+    layout(location = 0) in vec3 a_position;
+    layout(location = 1) in vec2 a_texcoord;
+    layout(location = 0) out vec2 v_texcoord;
+
+    void main() {
+        v_texcoord = a_texcoord;
+        gl_Position = vec4(a_position, 1.0);
+    }
+    "#
+};
+
+//language=glsl
+const FRAGMENT_SHADER: ShaderSource = notan::fragment_shader! {
+  r#"
+    #version 450
+    precision mediump float;
+
+    layout(location = 0) out vec4 outColor;
+    layout(location = 0) in vec2 v_texcoord;
+
+    layout(binding = 0) uniform sampler2D src;
+    layout(set = 0, binding = 0) uniform Locals {
+        float intensity;
+    };
+
+    void main() {
+        vec4 color = texture(src, v_texcoord);
+        color.a = pow(color.a, 1.0 / 2.2);
+        outColor = color;
+    }
+    "#
+};
+
+#[derive(Clone)]
+pub struct Workaround {
+    pipeline: Pipeline,
+    vertex_buffer: Buffer,
+    index_buffer: Buffer,
+    uniform_buffer: Buffer,
+}
+
+impl Workaround {
+    pub fn new(gfx: &mut Graphics) -> Self {
+        let vertex_info = VertexInfo::new()
+            .attr(0, VertexFormat::Float32x3)
+            .attr(1, VertexFormat::Float32x2);
+
+        let pipeline = gfx
+            .create_pipeline()
+            .from(&VERTEX_SHADER, &FRAGMENT_SHADER)
+            .with_color_blend(BlendMode::NORMAL)
+            .with_vertex_info(&vertex_info)
+            .with_texture_location(0, "src")
+            .build()
+            .unwrap();
+
+        #[rustfmt::skip]
+        let vertices = [
+            //pos               //coords
+            1.0,  1.0, 0.0,     1.0, 1.0,
+            1.0, -1.0, 0.0,     1.0, 0.0,
+            -1.0, -1.0, 0.0,    0.0, 0.0,
+            -1.0, 1.0, 0.0,    0.0, 1.0
+        ];
+
+        #[rustfmt::skip]
+        let indices = [
+            0, 1, 3,
+            1, 2, 3,
+        ];
+
+        let uniforms = [0.0]; // TODO: initialize with something better?
+
+        let vertex_buffer = gfx
+            .create_vertex_buffer()
+            .with_info(&vertex_info)
+            .with_data(&vertices)
+            .build()
+            .unwrap();
+
+        let index_buffer = gfx
+            .create_index_buffer()
+            .with_data(&indices)
+            .build()
+            .unwrap();
+
+        let uniform_buffer = gfx
+            .create_uniform_buffer(0, "Locals")
+            .with_data(&uniforms)
+            .build()
+            .unwrap();
+
+        Self {
+            pipeline,
+            vertex_buffer,
+            index_buffer,
+            uniform_buffer,
+        }
+    }
+
+    pub fn draw(
+        &self,
+        device: &mut Device,
+        src: &Texture,
+        dst: &RenderTexture,
+        uniform_data: Vec<f32>,
+    ) {
+        device.set_buffer_data(&self.uniform_buffer, &uniform_data);
+
+        let mut renderer = device.create_renderer();
+
+        renderer.begin(Some(&ClearOptions::color(Color::TRANSPARENT)));
+
+        renderer.set_pipeline(&self.pipeline);
+        renderer.bind_texture_slot(0, 0, src);
+        renderer.bind_buffers(&[
+            &self.vertex_buffer,
+            &self.index_buffer,
+            &self.uniform_buffer,
+        ]);
+        renderer.draw(0, 6);
+        renderer.end();
+
+        device.render_to(dst, renderer.commands());
+    }
+}
